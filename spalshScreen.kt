dd565d204fa1ac22cb9f98e1864520b839919dc9
Vishal <31177415+tech-deity@users.noreply.github.com>
2021-06-24 13:30:55 +0530

    Create spalshScreen.kt

---
+++ spalshScreen.kt
@@ -0,0 +1,22 @@
+package com.food.nourriture
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()},1500)
+    }
+}
